7f29719cfd994d080fcbe98e421375fbce2a89fa
PaoloTozzi <paolo.tozzi@aulab.it>
2024-12-23 20:23:28 +0100

    v3

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let num = document.querySelector('#num');
-let row = document.querySelector('#row');
 let btni = document.querySelector('#btni');
 let wrapper = document.querySelector('#wrapper');
 let btnRound = document.querySelector('#btnRound');
 let giro = document.querySelector('#giro');
+let poste = document.querySelector('#poste')
 
 let count = 1;
 localStorage.setItem('round', count);
@@ -29,25 +29,29 @@ function getPattern(number) {
 btni.addEventListener('click', () => {
     
     
-    let partial = getPattern(num.value);
+    let partial = Number(num.value) > 50 ? getPattern(num.value) : 0;
     let div = document.createElement('div');
-    let total = partial + 1500;
+    let total = (partial + 1500) - poste.value;
     
     wrapper.innerHTML = `` 
 
     div.innerHTML = `
-        <div class="container">
+        <div class="container mt-4">
             <div class="row">
                 <div class="col-12 col-6">
-                    <p>la banca ti deve ${partial}€ di interessi e 1500€ di stipendio</p>
+                    <p>la banca ti deve ${partial > 0 ? partial + "€ di interessi e ": ""} 1500€ di stipendio ${poste.value > 0 ? "e devi pagare " + poste.value + "€" : ""}</p>
                 </div>
                 <div class="col-12 col-6 mt-5">
-                    <p>Totale: ${total}€</p>
+                    <p class="bg-">Totale: ${total}€</p>
+                    ${poste.value > 0 ? "<p class='bg-danger'>Totale poste:" + poste.value + "€</p>": ""}
                     <p>Giro: ${count}</p>
                 </div>
             </div>
         </div>
     `
+
+    num.value = ""
+    poste.value = ""
     
     wrapper.appendChild(div);
     
